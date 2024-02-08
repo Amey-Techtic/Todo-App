d2d5e4a00868a21ce952ed0f70e2b7cf62233533
Amey-Techtic <amey@techtic.agency>
2024-02-08 13:55:23 +0530

    First commit

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
-let inputTitle= document.getElementById("title");
+let inputTitle = document.getElementById("title");
+let inputDetail = document.getElementById("detail");
+let listContainer = document.getElementById("listContainer");
+let addButton = document.getElementById("addDetails");
+let buttonText = document.getElementById("addDetails").innerText;
 
-let inputDetail= document.getElementById("detail");
+let todoArray = [];
+let todoObject = localStorage.getItem("todoData");
+let editId=null; //this will store id of edit element
 
-let listContainer= document.getElementById("listContainer");
-
-
-function addList(){
-   console.log(inputTitle.value,inputDetail.value);
-   if(inputTitle.value==='' || inputDetail===''){
-      alert("Please enter all task details");
-   }
-   else{
-      let list = document.createElement("li");
-      list.innerHTML=inputTitle.value + " : " + inputDetail.value;
-      listContainer.appendChild(list);
-      let span=document.createElement("span");
-      span.innerHTML='\u00d7';
-
-      list.appendChild(span);
-
-   }
-   inputTitle.value='';
-   inputDetail.value='';
-   saveData();
-
+if (todoObject != null) {
+  todoArray =
+    JSON.parse(todoObject); /*we will be storing old data through saveDetails() 
+   by localstorage getItem so that on page refresh, the old data is not lost*/
 }
 
-//remove element
-listContainer.addEventListener("click",function(e){
-   if(e.target.tagName==='SPAN'){
-      e.target.parentElement.remove();
-   }
-})
+displayDetails(); //display data in idle state also that is when we dont perform any click event 
 
-function saveData(){
-   localStorage.setItem("data",listContainer.innerHTML);
-   //as innerHTML of listContainer div is list li
+console.log(todoArray);
+
+addButton.onclick = () => {
+
+   const title = inputTitle.value;
+   const detail = inputDetail.value;
+
+  if(editId!=null){
+   //edit
+
+   todoArray.splice(editId,1,{title : title , detail : detail});
+   editId=null;
+  }
+  else{
+   //insert
+   
+   todoArray.push({ title: title, detail: detail }); //this will add data in last index of array;
+  }
+  
+  console.log(todoArray);
+  saveDetails(todoArray);
+  displayDetails();/*display data when user clicks add button, if we dont call then, we need to 
+  refresh page manually to display newly added data*/
+  inputTitle.value = "";
+  inputDetail.value = "";
+  addButton.innerText=buttonText;
+};
+
+function saveDetails(todoArray) {
+  let stringArray = JSON.stringify(todoArray); //as localstorage setItem allows vaue of only string type
+  localStorage.setItem("todoData", stringArray);
 }
 
-function showTask(){
-   listContainer.innerHTML=localStorage.getItem("data")
+function displayDetails() {
+  let listData = "";
+  todoArray.forEach((todo,i) => {
+    listData += `<li>${i+1} <h3 id="titleHead">${todo.title} -</h3><h4 id="detailHead"> ${todo.detail} </h4><span><i class="fa-solid fa-pen" id="editIcon" onClick='editInfo(${i})'></i> <i class="fa-solid fa-trash" id="deleteIcon" onClick='deleteInfo(${i})'></i></span></li>`;
+  });
+  listContainer.innerHTML=listData;
 }
 
-showTask();
+function editInfo(id){
+   editId=id;
+   inputTitle.value=todoArray[id].title;
+   inputDetail.value=todoArray[id].detail;
+   addButton.innerText='Save Changes';
+   console.log(todoArray);
+}
+
+function deleteInfo(id){
+   todoArray.splice(id,1); //so that this will remove elment of particular id from todoArray as splice affects original array
+
+   saveDetails(todoArray); //this should also save todoArray with removed element in localstorage so that it would not just remove list data from user interface but also from localstorage too;
+   
+   displayDetails(); // after removing element from todoArray, it must also reflect on user interface too   
+}
+
